0c66024246cad6481bac7abfc993584b44777731
Nathan Ohere <email>
2024-09-05 18:25:27 +0100

    Made changes

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12700,7 +12700,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64280" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56472" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
